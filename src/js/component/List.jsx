0bf97c9d77db81471fd85f6b31c6991d10ada648
sarahbejerano <bejeranosarah@gmail.com>
2021-10-21 01:07:32 +0000

    wip

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -4,19 +4,28 @@ import "bootstrap";
 //create your first component
 const TodoList = () => {
 	const [inputValue, setInputValue] = useState("text");
-	const [inputList, setInputList] = useState([ " ", " " , " " ]);
+	const [inputList, setInputList] = useState([" ", " ", " "]);
 
-	function addItem() {
-		setInputValue (inputValue);
-		const list = inputList;
-		setInputList(list);
-		console.log(TodoList);
-		
+	function addItem(e) {
+		//e.preventDefault(); this is only for buttons
+		if (e.onKeyPress == 13) {
+			setInputValue(inputValue);
+			const list = inputList.concat(inputValue);
+			setInputList(list);
+		}
 	}
+
 	return (
 		<div>
 			<h1>TODO List</h1>
-			<input type="text" id="list" />
+			<input
+				type="text"
+				id="list"
+				value={inputValue}
+				onChange={e => setInputValue(e.target.value)}
+				onKeyPress={addItem}
+			/>
+			<div>{inputList.map}</div>
 		</div>
 	);
 };
