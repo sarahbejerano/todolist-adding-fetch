4066c51837dc6838a27cf853f11dd71b665acf81
sarahbejerano <bejeranosarah@gmail.com>
2021-10-21 20:04:07 +0000

    wip

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import "bootstrap";
 
-//create your first component
 const TodoList = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [inputList, setInputList] = useState([]);
 
 	function addItem(e) {
-		//e.preventDefault(); this is only for buttons
-		console.log(e.key);
 		if (e.key == "Enter") {
 			const list = inputList.concat(inputValue);
 			setInputList(list);
 			setInputValue("");
 		}
+	}
+	function deleteItem(index) {
+		setInputList(list => list.splice(index, 1));
+		console.log(index);
 	}
 
 	return (
@@ -27,21 +28,20 @@ const TodoList = () => {
 				onKeyPress={addItem}
 				placeholder="What to do next?"
 			/>
-			<div>
-				{inputList.map((element, index) => {
-					return (
-						<>
-							<input
-								type="text"
-								key={element + index}
-								value={element}
-								readOnly
-							/>
-							<button>x</button>
-						</>
-					);
-				})}
-			</div>
+
+			{inputList.map((element, index) => {
+				return (
+					<>
+						<input
+							type="text"
+							key={element + index}
+							value={element}
+							readOnly
+						/>
+						<button onClick={() => deleteItem(index)}>x</button>
+					</>
+				);
+			})}
 		</div>
 	);
 };
