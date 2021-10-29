268df041aaec6ef881eb2d7ffbaafa8897fc5c28
sarahbejerano <bejeranosarah@gmail.com>
2021-10-29 23:41:49 +0000

    todolistok

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -21,21 +21,16 @@ const TodoList = () => {
 		});
 	}
 
-	// function deletelist() {
-	// 	setInputList(function(currentList) {
-	// 		return currentList.filter(function(el, elIndex) {
-	// 			return index !== elIndex;
-	// 		});
-	// 	});
-	// }
-
-	// function deletelist() {
-	// 	setInputList([]);
-	// }
-
 	return (
 		<div>
 			<h1>TODO List</h1>
+			<h3>
+				{inputList.length == 0
+					? "No task to display"
+					: inputList.length == 1
+					? "there is one task left"
+					: "The number of tasks left is" + " " + inputList.length}
+			</h3>
 			<input
 				type="text"
 				id="list"
@@ -44,23 +39,21 @@ const TodoList = () => {
 				onKeyPress={addItem}
 				placeholder="What to do next?"
 			/>
-			{inputList.map((element, index) => {
-				return (
-					<div key={element + index}>
-						<input type="text" value={element} />
-						<button
-							className="btn btn-outline-danger btn-sm"
-							onClick={() => deleteItem(index)}>
-							x
-						</button>
-					</div>
-				);
-			})}
-			<button
-				className="btn btn-outline-black btn-sm"
-				onClick={setInputList([])}>
-				delete all
-			</button>
+			<div>
+				{inputList.map((element, index) => {
+					return (
+						<div key={element + index}>
+							<input type="text" value={element} />
+							<button
+								className="btn btn-outline-danger btn-sm"
+								onClick={() => deleteItem(index)}>
+								x
+							</button>
+						</div>
+					);
+				})}
+			</div>
+			<button onClick={() => setInputList([])}>Delete All</button>
 		</div>
 	);
 };
